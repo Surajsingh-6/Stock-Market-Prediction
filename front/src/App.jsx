6034efa311753f973fde832d39a164d04983cdf4
Suraj singh <suraj142005@gamil.com>
2024-10-26 10:51:31 +0530

    frontend work

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import Navbar from './components/Navbar';
+import Usage from './components/Usage';
 function App() {
   const [file, setFile] = useState(null);
   const [date, setDate] = useState('');
@@ -11,11 +12,11 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    
-    const formData = new FormData(); 
+
+    const formData = new FormData();
     formData.append('file', file);
     formData.append('date', date);
-    formData.append('ltp', ltp); 
+    formData.append('ltp', ltp);
 
     try {
       const res = await axios.post('http://127.0.0.1:8000/getdata', formData); // Make sure the URL is correct
@@ -30,38 +31,42 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Stock Price Prediction</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Date:
-            <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Price:
-            <input type="text" value={ltp} onChange={(e) => setLtp(e.target.value)} /> {/* Changed to text for price */}
-          </label>
-        </div>
-        <div>
-          <label>
-            File:
-            <input type="file" onChange={(e) => setFile(e.target.files[0])} /> {/* Removed value prop */}
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-      {pred && (
+    <div className="absolute top-0 z-[-2] min-h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
+      <Navbar />
+      <div className='h-3/4 w-5/6 lg:w-2/4 p-2 m-auto my-10 border-[#ffffff] border-2'>
+        <h1 className='text-center mb-8 text-xl'>GET PREDICTIONS</h1>
+        <form onSubmit={handleSubmit} className='flex justify-center flex-col gap-5 p-2'>
           <div>
-            <h2>{pred.prediction}</h2>
-            <h2>{pred.accuracy}</h2>
+            <label >
+              <div className=' font-mono text-lg'>Date:</div>
+              <div className=' flex justify-center'><input className='text-black w-3/4 rounded-sm text-center  h-9' type="date" value={date} onChange={(e) => setDate(e.target.value)} /></div>
+            </label>
           </div>
-        )}
-        {loading && <h3>Loading...</h3>}
+          <div>
+            <label>
+              <div className=' font-mono text-lg'>Price:</div>
+              <div className='flex justify-center'> <input className='text-black w-3/4 rounded-sm text-center  h-9' type="text" value={ltp} onChange={(e) => setLtp(e.target.value)} /> {/* Changed to text for price */}</div>
+            </label>
+          </div>
+          <div>
+            <label>
+              <div className=' font-mono text-lg'>Upload File</div>
+              <div className='flex justify-center pl-5'><input type="file" onChange={(e) => setFile(e.target.files[0])} /> {/* Removed value prop */}</div>
+            </label>
+          </div>
+          <button className='bg-[#ffffff65] border-2 border-black w-fit m-auto p-1 rounded-md ' type="submit">Submit</button>
+        </form>
+        <div>
+          {pred && (
+            <div className='text-2xl ml-4'>
+              <h2>{pred.prediction}</h2>
+              <h2>{pred.accuracy}</h2>
+            </div>
+          )}
+          {loading && <h3>Loading...</h3>}
+        </div>
       </div>
+      <div id='click'><Usage />  </div>  
     </div>
   );
 }
